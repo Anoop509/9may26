0a45ba9bf7588f7a467a850b29e7f5dfc4f8f37d
Anoop509 <anoopkushwaha509@gmail.com>
2026-05-09 16:40:06 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.67.0"
+    }
+  }
+  backend "azurerm" {} 
+}
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = "e8d3e0f5-0b3e-4121-be0b-58a7e13e93e4"
+  features {}
+}
+
+resource "azurerm_resource_group" "anooprg" {
+  name     = "anooprg"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "anoop_sa" {
+  name                     = "anoopsa"
+  resource_group_name      = azurerm_resource_group.anooprg.name
+  location                 = azurerm_resource_group.anooprg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
